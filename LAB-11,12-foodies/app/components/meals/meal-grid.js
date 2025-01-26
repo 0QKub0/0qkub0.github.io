1fca446f278107eb02ee3c718b349e20ffc5af47
0QKub0 <qkubplay@gmail.com>
2025-01-26 17:05:45 +0100

    LAB13

--- LAB-11,12-foodies/app/components/meals/meal-grid.js
+++ LAB-11,12-foodies/app/components/meals/meal-grid.js
@@ -1,13 +1,14 @@
 import MealItem from "./meal-item";
-import styles from './meals-grid.module.css';
-export default function MealsGrid({meals}){
-    return(
-        <ul className={styles.meals}>
-            {meals.map((meal) => (
-                <li key={meals.id}>
-                    <MealItem {...meal} />
-                </li>
-            ))}
-        </ul>
-    );
+import styles from "./meals-grid.module.css";
+
+export default function MealsGrid({ meals }) {
+  return (
+    <ul className={styles.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
+      ))}
+    </ul>
+  );
 }
